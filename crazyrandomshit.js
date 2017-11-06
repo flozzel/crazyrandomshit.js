a6f8d639dfc30fbc9de1de138e117c5ddf480287
flozzel <33422121+flozzel@users.noreply.github.com>
2017-11-06 14:23:19 +0100

    Create crazyrandomshit.js

---
+++ crazyrandomshit.js
@@ -0,0 +1,32 @@
+var cursors =
+[
+"context-menu","help","pointer","progress","wait","cell","crosshair","text","vertical-text","alias","copy","move","no-drop","not-allowed",
+"e-resize","n-resize","ne-resize","nw-resize","s-resize","se-resize","sw-resize","w-resize","ew-resize","ns-resize","nesw-resize","nwse-resize",
+"col-resize","row-resize","all-scroll","zoom-in","zoom-out","grab","grabbing"
+];
+
+var elements = document.getElementsByTagName( '*' );
+
+setInterval(
+function()
+{
+	
+	var cursorIndex = Math.floor( ( Math.random() * cursors.length ) + 1);
+	var elementsIndex = Math.floor( ( Math.random() * elements.length ) + 1);
+	
+	var scrollX = Math.floor( ( Math.random() * 1000 ) + 1);
+	var scrollY = Math.floor( ( Math.random() * 1000 ) + 1);
+	
+	
+	element = elements[ elementsIndex ];
+	element.style.position = "absolute";
+	element.style.width = Math.floor( ( Math.random() * 100 ) + 1) + 'px';
+	element.style.height = Math.floor( ( Math.random() * 100 ) + 1) + 'px';
+	
+	element.style.top = Math.floor( ( Math.random() * 100 ) + 1) + 'px';
+	element.style.left = Math.floor( ( Math.random() * 100 ) + 1) + 'px';
+	
+	document.getElementsByTagName( 'html' )[ 0 ].style.cursor = cursors[ cursorIndex ];
+	//window.scrollTo( scrollX, scrollY );
+
+}, 100);
